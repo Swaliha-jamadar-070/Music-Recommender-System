18a07ca72cf6933d0aa8359db91fd8bbf796d589
Swaliha Jamadar <swalihajamadar070@gmail.com>
2026-04-21 13:19:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -250,5 +250,8 @@ def home():
                            trending=trending)
 
 # ================== RUN ==================
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
