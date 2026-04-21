12091d8c402c32fd3158860acdfcd3209c3174f9
Swaliha Jamadar <swalihajamadar070@gmail.com>
2026-04-21 16:21:11 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -6,29 +6,14 @@ function play(name, preview, artist){
         body:JSON.stringify({song:name})
     });
 
-    let player = document.getElementById("player");
+    let audio = document.getElementById("audio");
     let now = document.getElementById("now");
 
     if(preview){
-        player.src = preview;
-        player.play();
+        audio.src = preview;
+        audio.play();
         now.innerText = "🎶 " + name + " - " + artist;
     }else{
         window.open(`https://youtube.com/results?search_query=${name} ${artist}`);
     }
-}
-
-/* 🔥 YouTube style history click */
-function quickPlay(name, artist){
-    play(name, "", artist);
-}
-
-function like(name){
-    fetch("/like",{
-        method:"POST",
-        headers:{"Content-Type":"application/json"},
-        body:JSON.stringify({song:name})
-    });
-
-    alert("❤️ Added to Liked Songs");
 }
